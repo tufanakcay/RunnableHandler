0272a43ccee36930d11ea06bde722eeccef68cca
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-13 20:58:56 +0300

    Update MainActivity.java

--- app/src/main/java/com/tufanakcay/runnablehandler/MainActivity.java
+++ app/src/main/java/com/tufanakcay/runnablehandler/MainActivity.java
@@ -3,12 +3,26 @@ package com.tufanakcay.runnablehandler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int number;
+    Runnable runnable;
+    Handler handler;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        number = 0;
     }
+
 }
